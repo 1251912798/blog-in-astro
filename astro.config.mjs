5c541c163ba998aef3e9c60b84ecbc33fa3c8129
1251912798 <1251912798@qq.com>
2025-05-13 20:08:31 +0800

    chore: server配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    server: {
+        allowedHosts: [
+            "devserver-main--yinp.netlify.app", // 允许的域名
+            ".netlify.app", // 可选：允许所有 netlify.app 子域名
+            "localhost", // 保留本地开发支持
+        ],
+    },
+});
